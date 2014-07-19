a2bfad7527a4f531245883ecd5728c9177d400ed
James Hughes <james@yobriefca.se>
2014-07-19 10:42:37 +0100

    01 First Query
    We have created our first query and made yesql generate a function for
    it.  We also used the pprint method to print out our data in a more
    readable manner.

--- src/yesql_tinkering/core.clj
+++ src/yesql_tinkering/core.clj
@@ -1,8 +1,11 @@
 (ns yesql-tinkering.core
-  (:require [yesql.core :refer [defquery]]))
+  (:require [yesql.core :refer [defquery]]
+            [clojure.pprint :refer [pprint]]))
 
 (def db-spec {:classname    "org.h2.Driver"
               :subprotocol  "h2"
               :subname      "./resources/demo.db"
               :user         "sa"
               :password     "" })
+
+(defquery get-all-people "sql/get-all-people.sql")
